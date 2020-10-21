aacbb31f0b44b4d57909e07222f023ccfb3f8e21
zhangsongyang <zhangsongyang@haier.com>
2020-10-21 12:34:33 +0800

    go leetcode 925. 长按键入

--- simpleness/simp_main.go
+++ simpleness/simp_main.go
@@ -22,10 +22,13 @@ func main() {
 	fmt.Println("191. 位1的个数:", problem.HammingWeight(011))
 	// 231. 2的幂
 	fmt.Println("输入一个int:")
-	var n int
-	fmt.Scanln(&n)
+	//var n int
+	//fmt.Scanln(&n)
+	n := 25
 	fmt.Printf("231. 2的幂--> %v是否是 2 的幂次:%v\n", n, problem.IsPowerOfTwo(n))
 	// 190. 颠倒二进制位
-	fmt.Printf("190. 颠倒二进制位:%b", problem.ReverseBits(4294967293))
+	fmt.Printf("190. 颠倒二进制位:%b\n", problem.ReverseBits(4294967293))
+	// 925.长按键入
+	fmt.Printf("925.长按键入: %v\n", problem.IsLongPressedName("alex", "aaleex"))
 
 }
